59e991046980d33a5fc1519872493d33b76e8e3b
umutgulac <umut.gulac@ba-techs.com>
2017-10-02 19:27:28 +0300

    srvmon
    srvmon

---
+++ phpservermon-3.2.0/URL.php
@@ -0,0 +1,1 @@
+<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>
---
+++ phpservermon-3.2.0/config.php
@@ -0,0 +1,8 @@
+<?php
+define('PSM_DB_PREFIX', '');
+define('PSM_DB_USER', 'mysqldbuser');
+define('PSM_DB_PASS', 'Alisaid2261');
+define('PSM_DB_NAME', 'srvmon');
+define('PSM_DB_HOST', 'batechmysql.mysql.database.azure.com');
+define('PSM_DB_PORT', '3306');
+define('PSM_BASE_URL', 'http://srvmon.azurewebsites.net/phpservermon-3.2.0');
